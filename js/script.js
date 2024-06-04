7fba77b6aad8fe9864770b459bedcbd71cf1cf13
Matheus <matheusramosbi.13@gmail.com>
2024-06-04 19:45:32 -0300

    corrections & preventDefault of links

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+//Remover evento padrão dos links
+const allLinks = [...document.querySelectorAll("a")];
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
+});
+
 // Substituir texto de link interno
 const screen = window.innerWidth;
 const link = document.querySelector(".croissants-texto a");
