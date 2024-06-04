a940c3efc3e17ed3473ef002c0cee348463e37c9
Matheus <matheusramosbi.13@gmail.com>
2024-06-04 17:53:28 -0300

    add croissants article

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Substituir cursor pela div
 const divCursor = document.getElementById("cursor");
 
 document.addEventListener("mousemove", (event) => {
@@ -8,3 +9,11 @@ document.addEventListener("mousemove", (event) => {
   divCursor.style.top = Y - 6 + "px";
   divCursor.style.left = X - 6 + "px";
 });
+
+// Substituir texto de link interno
+const screen = window.innerWidth;
+const link = document.querySelector(".croissants-texto a");
+
+if (screen <= 1000) {
+  link.innerText = "Veja Todos >";
+}
