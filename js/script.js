e0f6d894237880e436db89047e26cae58a41c670
Matheus <matheusramosbi.13@gmail.com>
2024-06-04 18:38:05 -0300

    remove persistent div

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-// Substituir cursor pela div
-const divCursor = document.getElementById("cursor");
-
-document.addEventListener("mousemove", (event) => {
-  const Y = event.clientY;
-  const X = event.clientX;
-
-  divCursor.style.display = "block";
-  divCursor.style.top = Y - 6 + "px";
-  divCursor.style.left = X - 6 + "px";
-});
-
 // Substituir texto de link interno
 const screen = window.innerWidth;
 const link = document.querySelector(".croissants-texto a");
