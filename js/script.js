2ab4baaa3cdab7f9504eccf3c58fd2b129c23279
Matheus <matheusramosbi.13@gmail.com>
2024-06-06 11:22:10 -0300

    location section & correcions

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 //Remover evento padrão dos links
-const allLinks = [...document.querySelectorAll("a")];
+function preventEvent() {
+  const allLinks = [...document.querySelectorAll("a")];
 
-allLinks.forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
+  allLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+    });
   });
-});
+}
 
 // Substituir texto de link interno
-const screen = window.innerWidth;
-const link = document.querySelector(".croissants-texto a");
+function textChange() {
+  const screen = window.innerWidth;
+  const link = document.querySelector(".croissants-texto a");
 
-if (screen <= 1000) {
-  link.innerText = "Veja Todos >";
+  if (screen <= 1000) {
+    link.innerText = "Veja Todos >";
+  }
 }
+
+preventEvent();
+textChange();
