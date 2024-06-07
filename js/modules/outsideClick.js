537376e9f052b968226516b4d6c592d923942500
Matheus <matheusramosbi.13@gmail.com>
2024-06-07 00:55:52 -0300

    optmizations on all site

--- js/modules/outsideClick.js
+++ js/modules/outsideClick.js
@@ -1,24 +1,22 @@
-export default function outsideClick(element, events, callback) {
+export default function outsideClick(element, event, callback) {
   const html = document.documentElement;
   const outside = "data-outside";
 
   if (!element.hasAttribute(outside)) {
-    events.forEach((userEvent) => {
-      setTimeout(() => {
-        html.addEventListener(userEvent, handleOutsideClick);
-      });
+    setTimeout(() => {
+      html.addEventListener(event, handleOutsideClick, { once: true });
     });
-
-    element.setAttribute(outside, "");
   }
 
   function handleOutsideClick(event) {
     if (!element.contains(event.target)) {
       element.removeAttribute(outside);
-      events.forEach((userEvent) => {
-        html.removeEventListener(userEvent, handleOutsideClick);
-      });
-      callback();
+
+      html.removeEventListener(event, handleOutsideClick);
+
+      callback;
     }
   }
+
+  element.setAttribute(outside, "");
 }
